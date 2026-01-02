bf1fe1008e4489fd2cfa446953e8e9e289a6eb5a
bloom-07 <shrutiawasthi2007@gmail.com>
2026-01-02 15:44:28 +0530

    Deploy config change

--- config.py
+++ config.py
@@ -1,25 +1,21 @@
 import os
+import mysql.connector
 from dotenv import load_dotenv
 
-load_dotenv()  # loads .env
+# Load .env only locally
+if os.getenv("RAILWAY_ENVIRONMENT") is None:
+    load_dotenv()
 
 SECRET_KEY = os.getenv("SECRET_KEY")
+DEBUG = False  
 
-DEBUG = True
-
-# Database Config
-
-import mysql.connector
-# Database config
 DB_config = {
     "host": os.getenv("DB_HOST"),
     "user": os.getenv("DB_USER"),
     "password": os.getenv("DB_PASSWORD"),
     "database": os.getenv("DB_NAME"),
+    "port": int(os.getenv("DB_PORT", 3306)),
 }
-try:
-    conn = mysql.connector.connect(**DB_config)
-    print("Connected OK")
-    conn.close()
-except Exception as e:
-    print("Connection error:", e)
+
+def get_db_connection():
+    return mysql.connector.connect(**DB_config)
